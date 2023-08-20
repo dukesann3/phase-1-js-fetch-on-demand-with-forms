3d4c611140235924636831cfbf61164005214bd4
Duke <dukemorise33@gmail.com>
2023-08-20 15:06:03 -0700

    phase-1 fetch lab complete with movie summary and title

--- src/index.js
+++ src/index.js
@@ -1,5 +1,28 @@
 const init = () => {
-  
+  document.querySelector('section form').addEventListener('submit', function(event){
+    event.preventDefault();
+    const userInput = event.target['userInputSearchByID'].value;
+
+    fetch(`http://localhost:3000/movies/${userInput}`)
+    .then(response => response.json())
+    .then((jsonedResponse) => {
+        const {title, summary} = jsonedResponse;
+        const [searchedMovieTitle,searchedMovieSummary] = ['searchedMovieTitle','searchedMovieSummary']
+
+        const movieTitleDOM = SingleElementAndIDAndTextContentSetter('p',searchedMovieTitle,title);
+        const movieSummaryDOM = SingleElementAndIDAndTextContentSetter('p',searchedMovieSummary,summary);
+
+        document.querySelector('section#movieDetails h4').appendChild(movieTitleDOM);
+        document.querySelector('section#movieDetails p#movieSummary').appendChild(movieSummaryDOM);
+    })
+  })
+}
+
+function SingleElementAndIDAndTextContentSetter(DOMElement,idName,textContent){
+    const newDOMElement = document.createElement(DOMElement);
+    newDOMElement.setAttribute('id',idName);
+    newDOMElement.textContent = textContent;
+    return newDOMElement;
 }
 
 document.addEventListener('DOMContentLoaded', init);
